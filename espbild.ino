7b92f97bb1dec5eb88c2e5621306c00fca2e8451
Tschibo00 <ballhause@gmx.de>
2018-12-09 11:39:31 +0100

    Add files via upload

--- espbild.ino
+++ espbild.ino
@@ -9,12 +9,13 @@
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
+#include "sleigh.h"
 
 #define DATA_PIN    1
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    20*30
-#define BRIGHTNESS          96
+#define BRIGHTNESS          120
 #define FRAMES_PER_SECOND  30
 
 CRGB leds[NUM_LEDS];
@@ -59,15 +60,54 @@ void drawTree(uint8_t pos, uint8_t darken){
   
 }
 
-int pos=0;
+void drawBackground(unsigned char *bitmap, int pos, int width){
+  int p;
+  for (int y=0;y<20;y++)
+    for (int x=0; x<30; x++){
+      p=(y*width+((x+pos)%width))*3+8;
+      screen[y*30+x]=CRGB(bitmap[p],bitmap[p+1],bitmap[p+2]);
+    }
+}
+
+void drawBackgroundMasked(unsigned char *bitmap, int pos, int ypos, int height, int width){
+  int p;
+  CRGB c;
+  for (int y=0;y<height;y++)
+    for (int x=0; x<30; x++){
+      p=(y*width+((x+pos)%width))*3+8;
+      c=CRGB(bitmap[p],bitmap[p+1],bitmap[p+2]);
+      if (c!=CRGB(0,255,255))
+        screen[(y+ypos)*30+x]=c;
+    }
+}
+
+void drawSprite(unsigned char *bitmap, int x, int y, int width, int height){
+  int pos=8;
+  CRGB c;
+  for (int yy=y;yy<y+height;yy++)
+    for (int xx=x;xx<x+width;xx++){
+      if ((xx>=0)&&(xx<30)&&(yy>=0)&&(yy<20)){
+        c=CRGB(bitmap[pos],bitmap[pos+1],bitmap[pos+2]);
+        if (c!=CRGB(0,255,255))
+          screen[yy*30+xx]=c;
+      }
+      pos+=3;
+    }
+}
+
 void calcPicture(){
-  if (screen[pos]==CRGB(255,255,255))
-    screen[pos]=CRGB::Blue;
+  drawBackground(mountains_tif, millis()/400,95);
+  drawBackgroundMasked(backdrop_tif, millis()/200, 0, 20, 93);
+  drawBackgroundMasked(snow_tif, millis()/100, 15, 5, 95);
+ 
+  if ((millis()%500)>250)
+    drawSprite(sleigh_tif,0,5,30,15);
   else
-    screen[pos]=CRGB::White;
-  pos++;
-  if (pos==600)
-    pos=0;
+    drawSprite(sleigh2_tif,0,5,30,15);
+
+  drawBackgroundMasked(snow_tif, millis()/70, 18, 5, 95);
+  drawSprite(tree1_tif,50-((millis()/50)%100),3,13,17);
+  drawSprite(tree2_tif,50-((millis()/30)%110),3,13,17);
 }
 
 void transformPicture(){
